a0749b52d16ccae69f837190f07d1371c2c84c1e
michaelvincerra <michael.vincerra@gmail.com>
2017-10-10 18:05:18 -0700

    Adds new exercises to ch3 up to Functional Concepts, pure functions.

--- ch3.js
+++ ch3.js
@@ -79,33 +79,150 @@ console.log(urlFriendly);
 
 // NEW
 
-const string1 = "This is the tonight show with Jimmy Fallon"
-const urlFriendly1 = string.replace(/ /g, "-")
-console.log(urlFriendly1)
+// const string1 = "This is the tonight show with Jimmy Fallon"
+// const urlFriendly1 = string.replace(/ /g, "-")
+// console.log(urlFriendly1)
 
-// DOM construction using OLD way, imperative
+// // DOM construction using OLD way, imperative
 
-var target = document.getElementById('target');
-var wrapper = document.createElement('div'); 
-var headline = document.createElement('h1'); 
+// var target = document.getElementById('target');
+// var wrapper = document.createElement('div'); 
+// var headline = document.createElement('h1'); 
 
-wrapper.id = "welcome"; 
-headline.innerText = "Hello World";
+// wrapper.id = "welcome"; 
+// headline.innerText = "Hello World";
 
-wrapper.appendChild(headline); 
-target.appendChild(wrapper); 
+// wrapper.appendChild(headline); 
+// target.appendChild(wrapper); 
 
 // REACT method
-// DOM construction using REACT component
+// Declarative DOM construction using REACT component
 
-const { render } = ReactDOM
+// const { render } = ReactDOM
 
-const Welcome = () => (
-    <div id="welcome">
-        <h1>Hello World</h1>
-    </div>
-)
-render(
-    <Welcome />,
-    document.getElementById('target')
-)
+// const Welcome = () => (
+//     <div id="welcome">
+//         <h1>Hello World</h1>
+//     </div>
+// )
+// render(
+//     <Welcome />,
+//     document.getElementById('target')
+// )
+
+//Key concepts of functional programming: 
+// Immutability; purity; data transformation; higher-order functions; recursion
+
+// Immutability
+
+let color_lawn = {
+    title: "lawn",
+    color: "00FF00",
+    rating: 0,
+}
+
+// Build a function to rate colors
+// BAD WAY
+
+// function rateColor(color, rating) {
+//     color.rating = rating
+//     return color
+// }
+// console.log(rateColor(color_lawn, 5).rating)
+// console.log(color_lawn.rating)
+
+// Revise the rateColor so as not to change original 'color' object
+// BEST WAY
+
+let rateColor = function(color,rating) {
+    // Object.assign makes a copy
+    // It takes a blank object and overwrites rating
+    return Object.assign({}, color, {rating: rating})
+}
+console.log(rateColor(color_lawn, 5).rating)
+console.log(color_lawn.rating)
+
+// Now revise the above using => 
+
+// const rateColor = (color, rating) =>
+//     ({
+//         ...color,
+//         rating
+//     })
+
+let list = [
+    {title: "Rad Red"},
+    {title: "Lawn Green"},
+    {title: "Party Pink"}
+]
+// Create a function that will add colors to array, using PUSH
+// However, 'push' is a MUTABLE method
+
+// let addColor = function(title, colors) {
+//     colors.push({title: title})
+//     return colors;
+// }
+// console.log(addColor("Glam Silver", list).length)
+// console.log(list.length)
+
+// Revise above formula using instead: Array.concat
+
+const addColor = (title, array) =>array.concat({title})
+console.log(addColor("Glam Silver", list).length)
+console.log(list.length)
+
+// Revise above formula using Emerging JavaScript:
+// const addColor = (title, list) => [...list, {title}]
+
+// PURE FUNCTIONS: Returns a value that is computed based on its arguments
+
+// NOT A PURE FUNCTION
+
+// let frederick = {
+//     name: "Frederick Douglass",
+//     canRead: false,
+//     canWrite: false,
+// }
+// function selfEducate() {
+//     frederick.canRead = true
+//     frederick.canWrite = true
+//     return frederick
+// }
+// selfEducate()
+// console.log(frederick)
+
+// REVISED AS A PURE FUNCTION
+// This function is 'pure' because it computes the value based on the value that was sent to it: person.
+// It returns a NEW person object WITHOUT muting the argument sent to it. 
+
+const frederick = {
+    name: "Frederick Douglass",
+    canRead: false,
+    canWrite: false,
+}
+
+const selfEducate = person =>
+    // Note: () must appear because these always follow => notation
+    ({
+       ...person, 
+       canRead: true,
+       canWrite: true, 
+    })
+console.log(selfEducate(frederick))
+console.log(frederick)
+
+// IMPURE FUNCTION
+// This mutates the DOM -- BAD!
+
+function Header(text) {
+    let h1 = document.createElement('h1');
+    h1.innerText = text;
+    document.body.appendChild(h1);
+}
+Header("Header() caused side effects");
+
+// PURE FUNCTION
+// This does NOT mutate the DOM
+
+// const Header = (props) => <h1>{props.title}</h1>
+//In the HTML, you must add for it to run: <script type="text/jsx">
